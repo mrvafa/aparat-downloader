3d871dc6794b386b5d0e28dc2dd71d68bc9cd982
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-09-14 13:31:23 +0430

    Correct spelling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import urllib.parse 
+import urllib.parse
 import requests
 
 BASE = 'https://www.aparat.com'
@@ -13,14 +13,15 @@ url = 'https://www.aparat.com/jadi/videos'
 def get_download_link_from_video_page(video_page_url):
     res = requests.get(video_page_url)
     if res.status_code == 200:
-        video_page_soup = BeautifulSoup(res.text, 'html.parser') 
-        span_donwload_links = video_page_soup.find_all('li',attrs={'class':'menu-item-link link'})
-        a_download_link = span_donwload_links[len(span_donwload_links) - 1].find('a')
+        video_page_soup = BeautifulSoup(res.text, 'html.parser')
+        span_download_links = video_page_soup.find_all('li', attrs={'class': 'menu-item-link link'})
+        a_download_link = span_download_links[len(span_download_links) - 1].find('a')
         if 'href' in str(a_download_link):
             return a_download_link['href']
 
     else:
         print(f'Error while getting {video_page_url} {res.text}')
+
 
 driver = webdriver.Firefox(executable_path=GECKODRIVER_PATH)
 driver.get(url)
@@ -42,7 +43,7 @@ html_output = driver.execute_script(js_script_get_html_code)
 
 # Get video page links
 soup = BeautifulSoup(html_output, 'html.parser')
-list_of_channel_videos = soup.find_all('div', attrs={'class':'item grid-item'})
+list_of_channel_videos = soup.find_all('div', attrs={'class': 'item grid-item'})
 
 video_link_pages = []
 for video_section in list_of_channel_videos:
@@ -51,7 +52,6 @@ for video_section in list_of_channel_videos:
     link = inner_inner_div.find('a')
     if 'href' in str(link):
         video_link_pages.append(urllib.parse.urljoin(BASE, link['href']))
-
 
 file = open('output.txt', 'w')
 
