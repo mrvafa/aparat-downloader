5f956d4fcb92f1a3336c4336c52a9d3dc45f0b4c
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-09-14 12:11:21 +0430

    Update .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
+import urllib.parse 
 
+BASE = 'https://www.aparat.com'
 GECKODRIVER_PATH = '/home/mrvafa/Driver/geckodriver'
 url = 'https://www.aparat.com/jadi/videos'
 
@@ -23,6 +26,16 @@ while not match:
 js_script_get_html_code = 'return (document.documentElement.outerHTML);'
 html_output = driver.execute_script(js_script_get_html_code)
 
+# Get video page links
+soup = BeautifulSoup(html_output, 'html.parser')
+list_of_channel_videos = soup.find_all('div', attrs={'class':'item grid-item'})
 
+video_link_page = []
+for video_section in list_of_channel_videos:
+    inner_div = video_section.find('div')
+    inner_inner_div = inner_div.find('div')
+    link = inner_inner_div.find('a')
+    if 'href' in str(link):
+        video_link_page.append(urllib.parse.urljoin(BASE, link['href']))
 
 driver.close()
