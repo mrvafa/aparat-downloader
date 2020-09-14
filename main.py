2875abcf978521231f04942c8937d0362f4d7d70
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-09-14 11:43:16 +0430

    Get HTML of page

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ url = 'https://www.aparat.com/jadi/videos'
 driver = webdriver.Firefox(executable_path=GECKODRIVER_PATH)
 driver.get(url)
 
+# Scroll to end of file
 lenOfPage = driver.execute_script(
     "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match = False
@@ -18,5 +19,10 @@ while not match:
         "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     if lastCount == lenOfPage:
         match = True
+
+js_script_get_html_code = 'return (document.documentElement.outerHTML);'
+html_output = driver.execute_script(js_script_get_html_code)
+
+
 
 driver.close()
