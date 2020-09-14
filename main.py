bce45e33d252f9086c3b14c1ede6cb6f225c7d28
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-09-14 09:32:05 +0430

    Scroll to end

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 GECKODRIVER_PATH = '/home/mrvafa/Driver/geckodriver'
@@ -5,3 +7,14 @@ url = 'https://www.aparat.com/jadi/videos'
 
 driver = webdriver.Firefox(executable_path=GECKODRIVER_PATH)
 driver.get(url)
+
+lenOfPage = driver.execute_script(
+    "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+match = False
+while not match:
+    lastCount = lenOfPage
+    time.sleep(3)
+    lenOfPage = driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    if lastCount == lenOfPage:
+        match = True
