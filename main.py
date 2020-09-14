3e5f81a2f8a1eb345c341c4385ef9b9a2a98c421
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-09-14 13:29:30 +0430

    Write download links in to file

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import urllib.parse 
+import requests
 
 BASE = 'https://www.aparat.com'
 GECKODRIVER_PATH = '/home/mrvafa/Driver/geckodriver'
 url = 'https://www.aparat.com/jadi/videos'
+
+
+def get_download_link_from_video_page(video_page_url):
+    res = requests.get(video_page_url)
+    if res.status_code == 200:
+        video_page_soup = BeautifulSoup(res.text, 'html.parser') 
+        span_donwload_links = video_page_soup.find_all('li',attrs={'class':'menu-item-link link'})
+        a_download_link = span_donwload_links[len(span_donwload_links) - 1].find('a')
+        if 'href' in str(a_download_link):
+            return a_download_link['href']
+
+    else:
+        print(f'Error while getting {video_page_url} {res.text}')
 
 driver = webdriver.Firefox(executable_path=GECKODRIVER_PATH)
 driver.get(url)
@@ -30,12 +44,21 @@ html_output = driver.execute_script(js_script_get_html_code)
 soup = BeautifulSoup(html_output, 'html.parser')
 list_of_channel_videos = soup.find_all('div', attrs={'class':'item grid-item'})
 
-video_link_page = []
+video_link_pages = []
 for video_section in list_of_channel_videos:
     inner_div = video_section.find('div')
     inner_inner_div = inner_div.find('div')
     link = inner_inner_div.find('a')
     if 'href' in str(link):
-        video_link_page.append(urllib.parse.urljoin(BASE, link['href']))
+        video_link_pages.append(urllib.parse.urljoin(BASE, link['href']))
+
+
+file = open('output.txt', 'w')
+
+for video_link_page in video_link_pages:
+    link = get_download_link_from_video_page(video_link_page)
+
+    if link:
+        file.write(f'{link}\n')
 
 driver.close()
